8dc262b2f2b64d174078b7c1b3e3935764b23a2e
Muhannad Saeed Almutairi <muhnndsaeed@Muhannads-MacBook-Pro.local>
2022-10-20 15:21:51 +0300

    update

--- sudoku/lib/main.dart
+++ sudoku/lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'dart:math';
 
@@ -15,116 +14,118 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   @override
-
-
-final  myControlloer = TextEditingController();
+  final myControlloer = TextEditingController();
 // int randomNumber = Random().nextInt(10);
 
+  @override
+  void initState() {
+    super.initState();
 
- 
+    // Start listening to changes.
+    myControlloer.addListener(_printLatestValue);
+  }
 
   @override
-void initState() {
-  super.initState();
-
-  // Start listening to changes.
-  myControlloer.addListener(_printLatestValue);
-}
-@override
-void dispose() {
+  void dispose() {
     // Clean up the controller when the widget is removed from the
     // widget tree.
     myControlloer.dispose();
     super.dispose();
   }
 
-
   Widget build(BuildContext context) {
-  var rng = Random();
-  for (var i = 1; i < 10; i++) {
-    // print(rng.nextInt(9));
-  }
+    var rng = Random();
+    for (var i = 1; i < 10; i++) {
+      // print(rng.nextInt(9));
+    }
     return MaterialApp(
-
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar:AppBar(actions: [IconButton(
-            onPressed: (){},
-            icon: const Icon(Icons.restart_alt_sharp),
-
-          ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  rng;
+                });
+              },
+              icon: const Icon(Icons.restart_alt_sharp),
+            ),
           ],
-          ) ,
-          // floatingActionButton: FloatingActionButton.small(onPressed: (){}),
-          
-          body: Column(
-            children: [
-              Container(
-                child: Expanded(
-                  child: GridView.builder(
-                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 3,
-                      ),
-                      itemCount: 9,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Card(
-                            child: GridView.builder(
+        ),
+        // floatingActionButton: FloatingActionButton.small(onPressed: (){}),
+
+        body: Column(
+          children: [
+            Container(
+              child: Expanded(
+                child: GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                    ),
+                    itemCount: 9,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Card(
+                        child: GridView.builder(
                           gridDelegate:
                               const SliverGridDelegateWithFixedCrossAxisCount(
-                                crossAxisCount: 3,
+                            crossAxisCount: 3,
                           ),
                           itemCount: 9,
-                          itemBuilder: (BuildContext context,  int index) {
+                          itemBuilder: (BuildContext context, int index) {
                             return Container(
-                              
-                                // color: Color.fromARGB(255, 188, 189, 165),
-                                
-                                child: TextField(
-                                  
-                                  onChanged: (text) {
-                                    print('$text');
-                                    
-                                          },
-                                  
-                                  textAlign: TextAlign.center,
-                                  decoration: InputDecoration(
-                                    hintText: "${rng.nextInt(9)+1}",
-                                    enabledBorder: const OutlineInputBorder(
-                                      borderSide: BorderSide(color: Colors.black,width: 1),
-                                    ),
+                              // color: Color.fromARGB(255, 188, 189, 165),
 
+                              child: TextField(
+                                onChanged: (text) {
+                                  print('$text');
+                                },
+                                textAlign: TextAlign.center,
+                                decoration: InputDecoration(
+                                  // hintText: "${rng.nextInt(9) + 1}"
+                                  enabledBorder: const OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Colors.black, width: 1),
                                   ),
-                                  // controller: myControlloer,
-                                  
                                 ),
-                                
-                                );
+                                controller: TextEditingController(
+                                    text: "${rng.nextInt(9) + 1}"),
+                              ),
+                            );
                           },
-                        ),);
-                      }),
-                      
-                ),
-                    
-                  
+                        ),
+                      );
+                    }),
               ),
-              ElevatedButton(onPressed: (){}, child: const Text("Run"),),
-              SizedBox(height: 20,),
-              ElevatedButton(onPressed: (){clearText();}, child: const Text("clear"),),
-              SizedBox(height: 170,),
- ],
-          ),
-          
-          
-        
-        ),);
-      
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Run"),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                clearText();
+              },
+              child: const Text("clear"),
+            ),
+            SizedBox(
+              height: 170,
+            ),
+          ],
+        ),
+      ),
+    );
   }
+
   void clearText() {
     myControlloer.clear();
   }
-void _printLatestValue() {
-  print('Second text field: ${myControlloer.text}');
-}
 
-
+  void _printLatestValue() {
+    print('Second text field: ${myControlloer.text}');
+  }
 }
